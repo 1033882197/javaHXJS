4d6c72ae7ae8cf8211fb8b83ece86aca77229d45
Ray <1033882197@qq.com>
2022-03-04 16:28:10 +0800

    stream

--- src/main/java/com/stream/CreatingStream.java
+++ src/main/java/com/stream/CreatingStream.java
@@ -2,12 +2,10 @@ package com.stream;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class CreatingStream {
@@ -32,9 +30,47 @@ public class CreatingStream {
         Stream<BigInteger> integers = Stream.iterate(BigInteger.ZERO, n -> n.add(BigInteger.ONE));
         show("integers", integers);
 
+        List<String> words = Arrays.asList("ABCD", "baSdc", "cewqr");
+        Stream<String> lowercaseWords = words.stream().map(String::toLowerCase).peek(x->{
+            return;}
+        );
+        show("lowercaseWords", lowercaseWords);
+
+        Stream<String> firstLetters = words.stream().map(s -> s.substring(0, 1));
+        show("firstLetters",firstLetters);
+
+        String a = "abcdefg";
+        show("aaa", codePoints(a));
+
+        Stream<Stream<String>> result = words.stream().map(w -> codePoints(w));
+        show("stream in stream", result);
+
+        Stream<String> flatResult = words.stream().flatMap(w -> codePoints(w));
+        show("stream flatMap", flatResult);
+
+        Stream<String> uniqueWords = Stream.of("Ray","Ray","Ma").distinct();
+        //only one "Ray" is retained
+
+        Stream<String> longestFirst = words.stream().sorted(Comparator.comparing(String::length).reversed());
+        
+        Optional<String> largest = words.stream().max(String ::compareToIgnoreCase);
+        System.out.println(largest);
+
 
 
     }
+
+    public static Stream<String> codePoints(String s){
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < s.length()){
+            int j = s.offsetByCodePoints(i , 1);
+            result.add(s.substring(i, j));
+            i = j;
+        }
+        return result.stream();
+    }
+
     public static <T> void show(String title, Stream<T> stream){
         final int SIZE = 10;
         List<T> firstElements = stream.limit(SIZE + 1).collect(Collectors.toList());
